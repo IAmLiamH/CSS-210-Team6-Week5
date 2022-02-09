e979094f61fbefaba5e05c548233bfb5cae4d4dd
IAmLiamH <har21091@byui.edu>
2022-02-08 23:13:39 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,17 @@
+<<<<<<< HEAD
 from Jumper.assets import Director
 
 director = Director()
 director.start_game()
 
 # skydiver = skydiver()
+=======
+from Jumper.game import Skydiver
+>>>>>>> 250ec3d5536306247a7681d53b081fdc3eaf3734
 
+MySkydiver = Skydiver()
+#cartoon = [" ___", "/", "___", "\ ", "\ ", "/ ", "\ ", "/ ", "O", "/", "|", "\ ", "/", "\ ", "XXXXXXXX"]
+#MySkydiver.__display_cartoon(cartoon)
 
 
 
